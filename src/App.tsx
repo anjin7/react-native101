8b1cc65e04516eb5af3a1205f6000e2b7ced96db
_An_Jin_ <aji0719@gmail.com>
2022-08-15 20:06:27 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+
+    </Router>
+  );
+}
+
+export default App;
