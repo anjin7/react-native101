a4bda0e33c5315160d1c127eaef8d238b8fede27
_An_Jin_ <aji0719@gmail.com>
2022-08-16 01:40:45 +0900

    정리

--- src/theme.ts
+++
@@ -1,7 +0,0 @@
-import { DefaultTheme } from "styled-components";
-
-export const lightTheme: DefaultTheme = {
-    bgColor: "linear-gradient(135deg,rgb(252, 250, 242), rgb(171, 209, 231))",
-    boardColor: "gray",
-    cardColor: "white",
-};
