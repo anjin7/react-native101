eef4c60be351615e46725b0787179f7994bb9bf8
_An_Jin_ <aji0719@gmail.com>
2022-08-18 14:36:52 +0900

    2.0

--- src/firbase.js
+++ src/firbase.js
@@ -3,9 +3,12 @@
 // import "firebase/compat/auth";
 // import "firebase/compat/firestore";
 // import "firebase/compat/storage";
+// import "firebase/auth";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,6 +24,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export default app;
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
+export const authService = getAuth(firebaseApp);
