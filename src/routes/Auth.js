6499fadb8957b09f2669d77da1556ac73b8db23a
_An_Jin_ <aji0719@gmail.com>
2022-08-20 17:51:12 +0900

    2.4

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -11,6 +11,7 @@ function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -33,10 +34,10 @@ function Auth() {
         }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
-
+  const toggleAccount = () => setNewAccount((prev) => !prev);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -56,8 +57,15 @@ function Auth() {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
