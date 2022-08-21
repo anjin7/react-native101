560a2862c43b3b9454e0bb679369f995d77a59b4
_An_Jin_ <aji0719@gmail.com>
2022-08-21 21:13:08 +0900

    2.6 LogOut

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { getAuth } from "firebase/auth"
+import { useHistory } from "react-router-dom";
 
 function Profile() {
+  const history = useHistory();
+  const auth = getAuth();
+  const onLogOutClick = () => {
+    auth.signOut();
+    history.push("/");
+  };
   return (
-    <div>
-      Profile
-    </div>
-  )
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 }
 
 export default Profile;
