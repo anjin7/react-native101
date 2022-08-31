a647bcef89f63cfb5335ba45429d7e58868775ea
_An_Jin_ <aji0719@gmail.com>
2022-08-31 23:53:46 +0900

    5.1.1 photo

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,11 @@
 import React, { useState, } from "react";
 import { useHistory } from "react-router-dom";
 import { getAuth, updateProfile } from "firebase/auth";
-// import { collection, query, where, getDocs } from "firebase/firestore";
-// import { dbService } from "../firbase";
-
-
 
 function Profile({ userObj }) {
   const history = useHistory();
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const [newPhoto, setNewPhoto] = useState()
   const auth = getAuth();
   const onLogOutClick = () => {
     auth.signOut();
@@ -24,7 +21,26 @@ function Profile({ userObj }) {
   const onSubmit = async (event) => {
     event.preventDefault();
     if (userObj.displayName !== newDisplayName) {
-      await updateProfile(userObj, { displayName: newDisplayName });
+      await updateProfile(userObj, {
+        displayName: newDisplayName,
+        photoURL: newPhoto,
+      });
+    }
+  };
+    const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setNewPhoto(result);
+    };
+    if (theFile) {
+      reader.readAsDataURL(theFile);
     }
   };
 
@@ -37,8 +53,15 @@ function Profile({ userObj }) {
           placeholder="Display name"
           value={newDisplayName}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} ref={fileInput} />
         <input type="submit" value="Update Profile" />
         
+        {attachment && (
+          <div>
+            <img src={attachment} width="100px" height="100px" alt="img" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
