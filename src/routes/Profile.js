651da238a90cd2c354595b0d29fb94d7b41ef43f
_An_Jin_ <aji0719@gmail.com>
2022-08-31 17:57:53 +0900

    5.0

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,32 @@
-import React from "react";
-import { getAuth } from "firebase/auth"
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { dbService } from "../firbase";
 
-function Profile() {
+
+function Profile({ userObj }) {
   const history = useHistory();
   const auth = getAuth();
   const onLogOutClick = () => {
     auth.signOut();
     history.push("/");
   };
+
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", `${userObj.uid}`)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
