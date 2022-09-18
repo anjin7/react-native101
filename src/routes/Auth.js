9ef58252bb7b91f38ba34db899cd03f6e2c9f730
_An_Jin_ <aji0719@gmail.com>
2022-09-18 13:55:40 +0900

    6.2.4

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -7,13 +7,15 @@ import {
 } from 'firebase/auth';
 import AuthForm from '../components/AuthForm';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faGoogle,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Container = styled.div`
-  width: 360px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  /* border: 1px solid red; */
+
 `;
 
 function Auth() {
@@ -32,14 +34,20 @@ function Auth() {
     console.log(data);
   };
   return (
-    <Container>
+    <Container className="authContainer">
+      <FontAwesomeIcon
+        icon={faTwitter}
+        color={"#04AAFF"}
+        size="3x"
+        style={{ marginBottom: 30 }}
+      />
       <AuthForm />
-      <div>
-        <button onClick={onSocialClick} name="google">
-          Continue with Google
+      <div className="authBtns">
+        <button onClick={onSocialClick} name="google" className="authBtn">
+          Continue with Google <FontAwesomeIcon icon={faGoogle} />
         </button>
-        <button onClick={onSocialClick} name="github">
-          Continue with Github
+        <button onClick={onSocialClick} name="github" className="authBtn">
+          Continue with Github <FontAwesomeIcon icon={faGithub} />
         </button>
       </div>
     </Container>
